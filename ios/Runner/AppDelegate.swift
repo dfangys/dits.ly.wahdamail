5307a988f76dc8775a9acb8eaf625f0497f8f878
mianazeemdaula <mianazeemdaula@gmail.com>
2024-02-28 18:02:23 +0500

    ios-1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+      SwiftFlutterBackgroundServicePlugin.taskIdentifier = "id.newmail.background"
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
